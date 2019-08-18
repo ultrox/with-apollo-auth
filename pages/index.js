39ef7d0aaae156f7c1313f247a8082fb1d3984fb
Marko <crashxx@gmail.com>
2019-08-18 21:36:18 +0200

    Remove 'with-apollo' related components

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,1 @@
-import App from '../components/App'
-import Header from '../components/Header'
-import Submit from '../components/Submit'
-import PostList from '../components/PostList'
-
-export default () => (
-  <App>
-    <Header />
-    <Submit />
-    <PostList />
-  </App>
-)
+export default () => <div>Hello</div>
